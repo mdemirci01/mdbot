87876a5a0c3341e3f64628d3771b47a43b2f0db5
Murat Demirci <mdemirci@outlook.com>
2017-11-24 16:36:29 +0300

    localization

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
             var message = await argument;
-            await context.PostAsync("You said: " + message.Text);
+            await context.PostAsync(message.Text + " dedin");
             context.Wait(MessageReceivedAsync);
         }
     }
